b8bf56037dd401e21cd619f81f6fb6c9bf4ee431
Leslie-Abm <78493340+Leslie-Abm@users.noreply.github.com>
2021-02-26 14:48:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('nav.deuxiemenav');
+const liens = document.querySelectorAll('nav.deuxiemenav a');
+
+burger.addEventListener('click', function(){
+    menu.classList.toggle('menu-open');
+});
+
+liens.forEach((lien) => {
+    lien.addEventListener('click', function () {
+        menu.classList.remove('nav-open');
+    } );
+}
+)
+
+const tl = gsap.timeline({defaults:{ease:'power1.out'}});
+
+tl.to(".text",{y:'0%', duration:1, stagger:0.1});
+tl.to(".slider",{y:"-100%", duration:1.5, delay:1});
+tl.to(".intro",{y:"-100%", duration:1}, "-=1.1");
+tl.fromTo("nav",{opacity:0},{opacity:1, duration:1});
+
+
+class Carousel {
+    constructor (element, options= {})
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+Carousel (document.querySelector('#carousel1'), {
+    slidesToScroll: 2,
+    slidesVisible: 2 
+})
+
+})
